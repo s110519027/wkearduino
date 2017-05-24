5d351ac9cc44a0f5d3fbc41b74ddd93008883baf
s110519027 <ben0939637@gmail.com>
2017-05-24 10:35:35 +0800

    Add files via upload

---
+++ sketch_may24a/sketch_may24a.ino
@@ -0,0 +1,27 @@
+const int analogInPin=A0;
+const int analogOutPin=9;
+
+int sensorValue = 0;
+int outputValue = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  // 
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(analogInPin);
+  outputValue = map(sensorValue,960,600,255,0);
+
+  analogWrite(analogOutPin,outputValue);
+
+  Serial.print("sensor=");
+  Serial.print(sensorValue);
+  Serial.print("output=");
+  Serial.print(outputValue);
+    Serial.print("\n");
+
+  delay(2);
+}
